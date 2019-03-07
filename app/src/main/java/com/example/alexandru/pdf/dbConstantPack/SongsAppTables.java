b4f8c292a04f6401500b684c4400c108e2404f51
Alexandru Burghelea <burghelea.alex@gmail.com>
2019-03-07 22:40:40 +0200

    added column to database to help with search also change the main activity layout

--- app/src/main/java/com/example/alexandru/pdf/dbConstantPack/SongsAppTables.java
+++ app/src/main/java/com/example/alexandru/pdf/dbConstantPack/SongsAppTables.java
@@ -12,5 +12,6 @@ public final class SongsAppTables {
         public static final String COLUMN_SONG_TITLE = "song_title";
         public static final String COLUMN_SONG_TEXT = "song_text";
         public static final String COLUMN_song_category = "song_category";
+        public static final String COLUMN_SONG_TITLE_NO_ROM ="song_title_no_rom";
     }
 }
